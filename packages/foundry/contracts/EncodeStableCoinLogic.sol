aa20fb0c9031dcadf1f8942f33e690ba1605d6ea
--replace <tejumoladavid@gmail.com>
2024-09-29 18:36:04 -0400

    (contract): implement deposit collateral and test

--- packages/foundry/contracts/EncodeStableCoinLogic.sol
+++ packages/foundry/contracts/EncodeStableCoinLogic.sol
@@ -3,12 +3,13 @@
 pragma solidity 0.8.27;
 
 contract EncodeStableCoinLogic {
+    mapping(address => uint256) public collateralAmount;
+
     error AmountMustBeMoreThanZero();
     error ZeroAddress();
 
-
     constructor() {}
-    
+
     function mintEUSD(address _to, uint256 _amount) external {
         require (_to != address(0), ZeroAddress());
         require (_amount > 0, AmountMustBeMoreThanZero()); 
@@ -18,10 +19,25 @@ contract EncodeStableCoinLogic {
     }
 
     
-    function addCollateral() external  {
+    /**
+     * @dev This function is used to deposit collateral to the vault.
+     * msg.value is the amount of collateral to deposit
+     * msg.sender is the user who is depositing the collateral
+     */
+    function addCollateral() external payable {
+        collateralAmount[msg.sender] += msg.value;
     }
 
     function redeemCollateral() external {
+    }
+
+    /**
+     * @dev This function is used to check the collateral amount for a user.
+     * @param _user The address of the user to check the collateral amount for.
+     * @return The collateral amount for the user.
+     */
+    function checkCollateralOf(address _user) external view returns (uint256) {
+        return collateralAmount[_user];
     }
 
     function liquidate() external {
